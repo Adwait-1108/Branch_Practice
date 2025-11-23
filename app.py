0f2eecf44ecf26456d9b2fbb9ee514c055f6436c
Adwait <adwaitvid@gmail.com>
2025-11-23 12:35:57 +0530

    app.py updated

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ def even_odd(n):
         return "Even"
     else:
         return "Odd"
-    
-print("Hello There!!!")
+        
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else: 
+        return n * factorial(n-1)
+print("Hello There")
